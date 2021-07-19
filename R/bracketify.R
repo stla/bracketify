b3fb8cc3886f4eff39f94cf4dab9aa2a9e780b57
Stéphane Laurent <STEPHANE.X.LAURENT@GSK.COM>
2021-07-19 12:42:57 +0200

    first commit

---
+++ R/bracketify.R
@@ -0,0 +1,37 @@
+#' @importFrom rstudioapi hasFun getSourceEditorContext setDocumentContents
+#' @keywords internal
+bracketifyFile <- function(){
+  if(hasFun("getSourceEditorContext")){
+    editorContext <- getSourceEditorContext()
+  }else{
+    stop("This version of RStudio is too old!")
+  }
+  contents <- paste0(editorContext[["contents"]], collapse = "\n")
+  newContents <- gsub(
+    "(\\w*)\\$(`?)( *\\w+(?:[._\\- ]+\\w+)* *)\\2", '\\1[["\\3"]]',
+    contents, perl=TRUE
+  )
+  setDocumentContents(newContents, editorContext[["id"]])
+}
+
+#' @importFrom rstudioapi hasFun getSourceEditorContext modifyRange
+#' @keywords internal
+bracketifySelection <- function(){
+  if(hasFun("getSourceEditorContext")){
+    editorContext <- getSourceEditorContext()
+  }else{
+    stop("This version of RStudio is too old!")
+  }
+  selection <- editorContext[["selection"]][[1L]]
+  text <- selection[["text"]]
+  if(text == ""){
+    message("Nothing selected.")
+    return(invisible(NULL))
+  }
+  newText <- gsub(
+    "(\\w*)\\$(`?)( *\\w+(?:[._\\- ]+\\w+)* *)\\2", '\\1[["\\3"]]',
+    text, perl=TRUE
+  )
+  modifyRange(selection[["range"]], newText, editorContext[["id"]])
+}
+
---
+++ inst/essais/essai00.R
@@ -0,0 +1,11 @@
+a <- input$foo
+
+b <- input$foo + input$bar
+
+c <- input$`foo bar`
+
+d <- input$foo$bar
+
+e <- input$foo.bar
+
+f <- input$`b  a  z`; g <- input$q_u_x[["foo"]]
